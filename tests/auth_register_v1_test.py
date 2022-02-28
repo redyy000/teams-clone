8d6027a8f875a1ddd58a904049522080aa008700
Richard Xue <vinegar@Richards-MBP.hub>
2022-03-01 01:39:47 +1100

    Version 2 of the tests, updated to fix errors and new tests added

--- tests/auth_register_v1_test.py
+++ tests/auth_register_v1_test.py
@@ -1,38 +1,37 @@
 import pytest
 
-from src.auth import auth_register_v1
+from src import auth
 from src.error import InputError
 from src.other import clear_v1
+from src.data_store import data_store
 
 
 
-# Test an error occurs with a non-regular exp email
+# Test an error occurs with a non-regular expression email
 def test_email_invalid():
     clear_v1()
     with pytest.raises(InputError):
         # Non-Regular Expression Email
-        assert (auth_register_v1("000000", "Password", "First", "Last"))
+        assert (auth.auth_register_v1("@@@@@", "Password", "First", "Last"))
+        assert (auth.auth_register_v1("0000", "Password", "First", "Last"))
         
-    with pytest.raises(InputError):
-        # Non-Regular Expression Email
-        assert (auth_register_v1("@@@@@", "Password", "First", "Last"))
   
 
 # Normal Tests to test valid inputs and output ids
 # Output ids should be in ascending order from 1,2,3....
 def test_valid():
     clear_v1()
-    assert (auth_register_v1("john@gmail.com", "johndarksoul", "John", "Darksoul")) == 1 
-    assert (auth_register_v1("54321abba12345@hotmail.com", "TakeaChance0nMe", "Abby", "Bartholomew")) == 2
-    assert (auth_register_v1("z5555550@ad.unsw.edu.au", "Noma1dens?!#.(03e?&", "James", "Eldenring")) == 3
+    assert (auth.auth_register_v1("john@gmail.com", "johndarksoul", "John", "Darksoul")) == {'auth_user_id' : 1}
+    assert (auth.auth_register_v1("54321abba12345@hotmail.com", "TakeaChance0nMe", "Abby", "Bartholomew")) == {'auth_user_id' : 2}
+    assert (auth.auth_register_v1("z5555550@ad.unsw.edu.au", "Noma1dens?!#.(03e?&", "James", "Eldenring")) == {'auth_user_id' : 3}
     
 # Test that the email already exists
 def test_email_exists():
     clear_v1()
-    assert (auth_register_v1("George@gmail.com", "monkey", "George", "Monkey")) == 1 
+    assert (auth.auth_register_v1("George@gmail.com", "monkey", "George", "Monkey")) == {'auth_user_id' : 1}
     with pytest.raises(InputError):
         # Attempting to register with already used email.
-        assert (auth_register_v1("George@gmail.com", "banana", "George", "Banana"))
+        assert (auth.auth_register_v1("George@gmail.com", "banana", "George", "Banana"))
 
     
 # Test password is 5 or less letters long
@@ -41,13 +40,11 @@ def test_password_length_short():
     clear_v1()
     with pytest.raises(InputError):
         # 3 Letter long password
-        assert(auth_register_v1("normal@gmail.com", "abc", "John", "Smith"))
-    with pytest.raises(InputError):
+        assert(auth.auth_register_v1("normal@gmail.com", "abc", "John", "Smith"))
         # 5 Letter long password
-        assert(auth_register_v1("normal@gmail.com", "abcde", "John", "Smith"))
-    with pytest.raises(InputError):
+        assert(auth.auth_register_v1("normal@gmail.com", "abcde", "John", "Smith"))
         # 0 Letter long password
-        assert(auth_register_v1("normal@gmail.com", "", "John", "Smith"))
+        assert(auth.auth_register_v1("normal@gmail.com", "", "John", "Smith"))
 
 
 
@@ -55,23 +52,43 @@ def test_password_length_short():
 def test_name_first_length_long():
     clear_v1()
     with pytest.raises(InputError):
-        assert(auth_register_v1("normal@gmail.com", "abc", "mgubpezlxzrktxamqbrgizwdptqveadaykuffmplqnqiousnsrf", "Smith"))
+        assert(auth.auth_register_v1("normal@gmail.com", "abc", "mgubpezlxzrktxamqbrgizwdptqveadaykuffmplqnqiousnsrf", "Smith"))
 # Test name_first is 0 characters long
 
 def test_name_first_length_short():
     clear_v1()
     with pytest.raises(InputError):
-        assert(auth_register_v1("normal@gmail.com", "abc", "", "Smith"))
+        assert(auth.auth_register_v1("normal@gmail.com", "abc", "", "Smith"))
 
 # Test name_last is 51 characters long
 def test_name_last_length_long():
     clear_v1()
     with pytest.raises(InputError):
-        assert(auth_register_v1("normal@gmail.com", "abc", "John", "mgubpezlxzrktxamqbrgizwdptqveadaykuffmplqnqiousnsrf"))
+        assert(auth.auth_register_v1("normal@gmail.com", "abc", "John", "mgubpezlxzrktxamqbrgizwdptqveadaykuffmplqnqiousnsrf"))
     
 # Test name_last 0 characters long
 def test_name_last_length_short():
     clear_v1()
     with pytest.raises(InputError):
-        assert(auth_register_v1("normal@gmail.com", "abc", "John", ""))
+        assert(auth.auth_register_v1("normal@gmail.com", "abc", "John", ""))
+
+# Test for duplicate handles
+# NOT BLACKBOX
+# How can I solve extracting handle_str from a given user?
+# Using create_handle_str just kicks the problem down
+def test_handle_duplicate():
+    pass
+    '''
+    clear_v1()
+    store = data_store.get()
+    
+    handle_one = auth.create_handle_str()
+    auth.auth_register_v1("one@gmail.com", "Password", "John", "Smith")
+    auth.auth_register_v1("two@gmail.com", "Password", "John", "Smith")
+    auth.auth_register_v1("three@gmail.com", "Password", "John", "Smith")
+    
+    assert(store['users'][0]['handle_str']) == "johnsmith"
+    assert(store['users'][1]['handle_str']) == "johnsmith0"
+    assert(store['users'][2]['handle_str']) == "johnsmith1"
+    '''
 
