184a632558ab9b0a7d93517f05b0096bb87cd662
Max <z5309586@ad.unsw.edu.au>
2022-03-01 18:47:47 +1100

    Version 3 of test file that reflects expected parameters and return variables

--- tests/channel_messages_v1_test.py
+++ tests/channel_messages_v1_test.py
@@ -1,27 +1,73 @@
-from src.channel import channel_messages_v1
-from src.channel import channel_details_v1
-from src.channel import channel_join_v1
-from src.error import InputError, AccessError
-from src.data_store import data_store
 import pytest
+from src.channels import channels_create_v1
+from src import error
+from src.other import clear_v1
+from src.auth import auth_register_v1, auth_login_v1
+from src.channel import channel_invite_v1, channel_messages_v1
 
-##function prototype
-##def channel_messages_v1(auth_user_id, channel_id, start):
-
-
-##test channel_id does not refer to valid channel
+# Test the channel id is invalid
 def test_channel_id_invalid():
-    pass
-
-##test channel start is greater than total number of messages in channel
-def test_channel_index_invalid():
+    clear_v1()
+    user_id = auth_register_v1("max@gmail.com", "ovington", "Ox", "Movington")
+    user_id_2 = auth_register_v1("Xue@gmail.com", "password", "Richard", "Nixon")
+    user_id_3 = auth_register_v1("chicken@gmail.com", "AIENSW", "Marc", "Chee")
+    new_channel = channels_create_v1(user_id, "Discord", 1)
+    channel_invite_v1(user_id_2, new_channel, user_id)
     with pytest.raises(InputError):
-        channel_messages_v1("normal@gmail.com", {"auth_user_id": ["normal@gmail.com"], "messages": ["hello"]}, 70)
-        channel_messages_v1("normal@gmail.com", {"auth_user_id": ["normal@gmail.com"], "messages": ["hello", "goodbye"] }, 10)
-        channel_messages_v1("normal@gmail.com", {"auth_user_id": ["normal@gmail.com"], "messages": []}, -20 )
+        assert channel_messages_v1(user_id, 500, 0)
+        assert channel_messages_v1(user_id_2, "invalid", 0) 
+    
 
-##test when user is not member of channel
-def channel_member_invalid():
+# Test the channel index is greater than the message list or invalid
+def test_channel_index_invalid():
+    clear_v1()
+    user_id = auth_register_v1("max@gmail.com", "ovington", "Ox", "Movington")    
+    new_channel = channels_create_v1(user_id, "Reddit", 1)
+    with pytest.raises(InputError):
+        assert channel_messages_v1(user_id, new_channel, 20)
+        assert channel_messages_v1(user_id, new_channel, -200) 
+        assert channel_messages_v1(user_id, new_channel, "notanint")     
+    
+# Test the user is valid but not in channel or is an invalid input
+def test_channel_member_invalid():
+    clear_v1()
+    user_id = auth_register_v1("test@gmail.com", "12345", "Hayden", "Smith")
+    user_id_2 = auth_register_v1("hello@gmail.com", "password", "Jake", "Renzella")
+    user_id_3 = auth_register_v1("chicken@gmail.com", "AIENSW", "Marc", "Chee")
+    new_channel = channels_create_v1(user_id, "Netflix", 1)
+    channel_invite_v1(user_id_2, new_channel, user_id)
     with pytest.raises(AccessError):
-        channel_messages_v1("empty@gmail.com", {"auth_user_id": ["normal@gmail.com"], "messages": ["hello"]}, 0)
-        channel_messages_v1("test@gmail.com", {"auth_user_id": ["max@gmail.com", "richard@gmail.com", "tayla@gmail.com", "daniel@gmail.com", "ryan@gmail.com"], "messages": ["hello"]}, 0)
+        assert channel_messages_v1(200, new_channel, 0)
+        assert channel_messages_v1(200, new_channel, 0)
+        assert channel_messages_v1("invalid", new_channel, 0)        
+    
+    
+# Test successful message 
+def test_channel_message_success():
+    clear_v1()
+    user_id = auth_register_v1("123@gmail.com", "12345", "Number", "Man")
+    user_id_2 = auth_register_v1("567@gmail.com", "342984", "Count", "Dracula")
+    user_id_3 = auth_register_v1("8994@gmail.com", "AIENSW", "Kanye", "West")
+    new_channel = channels_create_v1(user_id, "7Prime", 1)
+    channel_invite_v1(user_id_2, new_channel, user_id)
+    assert channel_messages_v1(user_id, new_channel, 0) == {"messages": [], "start": 0, "end": -1}
+    assert channel_messages_v1(user_id_2, new_channel, 0) == {"messages": [], "start": 0, "end": -1}  
+    clear_v1()
+    user_id = auth_register_v1("123@gmail.com", "12345", "Number", "Man")
+    user_id_2 = auth_register_v1("567@gmail.com", "342984", "Count", "Dracula")
+    user_id_3 = auth_register_v1("8994@gmail.com", "AIENSW", "Kanye", "West")
+    new_channel = channels_create_v1(user_id, "7Prime", 1)
+    channel_invite_v1(user_id_2, new_channel, user_id)
+    channel_invite_v1(user_id_3, new_channel, user_id)
+    assert channel_messages_v1(user_id_1, new_channel, 0) == {"messages": [], "start": 0, "end": -1}
+    assert channel_messages_v1(user_id_2, new_channel, 0) == {"messages": [], "start": 0, "end": -1}
+    assert channel_messages_v1(user_id_3, new_channel, 0) == {"messages": [], "start": 0, "end": -1}
+    clear_v1()
+    user_id = auth_register_v1("123@gmail.com", "12345", "Number", "Man")
+    user_id_2 = auth_register_v1("567@gmail.com", "342984", "Count", "Dracula")
+    new_channel = channels_create_v1(user_id, "7Prime", 1)
+    new_channel_2 = channels_create_v1(user_id, "Foxtel", 1)
+    channel_invite_v1(user_id_2, new_channel, user_id)
+    channel_invite_v1(user_id_2, new_channel_2, user_id)
+    assert channel_messages_v1(user_id_2, new_channel, 0) == {[], 0, -1}
+    assert channel_messages_v1(user_id_2, new_channel_2, 0) == {[], 0, -1}  
