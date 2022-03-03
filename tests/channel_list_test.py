297f910777c9c8c60e0bc1e0cc09ae8601dee61d
Ryan Godakanda <ryangodakanda@gmail.com>
2022-03-03 13:04:14 +1100

    Corrected InputError to AccessError

--- tests/channel_list_test.py
+++ tests/channel_list_test.py
@@ -1,18 +1,11 @@
 import pytest
-
-from src.auth import auth_register_v1
 from src.channels import channels_list_v1
-from src.error import InputError
+from src.error import AccessError
 from src.other import clear_v1
-
-def auth_user_id():
-    clear_v1()
-    auth_user_id = auth_register_v1("Testemail@gmail.com", "testpassword22", "first", "last")['auth_user_id']
-    return auth_user_id
 
 def test_invalid_user_id():
     clear_v1()
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         assert channels_list_v1(0)
         assert channels_list_v1("Invalid ID")
         assert channels_list_v1(1)
