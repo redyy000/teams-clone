57664babdf712e33dbcf248904986b670ce12351
Max <z5309586@ad.unsw.edu.au>
2022-03-17 23:18:38 +1100

    added persistence and http wrapping for clear_v2

--- src/other.py
+++ src/other.py
@@ -1,7 +1,50 @@
+import pickle
+import src.server
+import requests
+from Flask import flask, request
+from pathlib import Path
 from src.data_store import data_store
+from json import dumps
+    
+BASE_URL = "http://127.0.0.1:{config.port}"
 
+def store_data(data):
+    '''
+    Input Types:
+    data --> dicionary (may be empty)
+    
+    Given a set of data, overwrite data.p with new data
+    '''
+    with open("data.p", "wb") as W_FILE:
+        W_FILE.write(pickle.dumps(data))
+
+@APP.route("/clear/v2", methods = ["DELETE"])
 def clear_v1():
-    store = data_store.get()
-    store['users'] = []
-    store['channels'] = []
-    data_store.set(store)
+    '''
+    Input Types:
+    None
+    
+    Sets data in data.p to a default dictionary of empty lists
+    '''
+    store = load_data()
+    DATA_STRUCTURE = {
+        "users": [],
+        "channels": [],
+        "dms": [],
+        "global_messages": [],
+        "tokens": []
+    }
+    with open("data.p", "wb") as W_FILE:
+        W_FILE.write(pickle.dumps(data))  
+
+def load_data():
+    '''
+    Input Types:
+    None
+    
+    load_data from data.p as a readable data structure
+    '''
+    if Path("data.p").exists() == False:
+        requests.delete(f"{BASE_URL}/clear/v2")
+    with open("data.p", "rb") as FILE:
+        return pickle.loads(FILE.read())
