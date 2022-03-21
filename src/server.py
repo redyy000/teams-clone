5715e17d5fc4f9dd9224842d94f8dcac80d03d4a
Max Ovington <m.ovington@student.unsw.edu.au>
2022-03-21 14:03:42 +1100

    Update server.py

--- src/server.py
+++ src/server.py
@@ -7,12 +7,33 @@ from other import token_create, token_decode
 from json import dumps
 import jwt
 
-
 APP = Flask(__name__)
 
 # return dumps({}) if empty 
 # must return smth
 
+def quit_gracefully(*args):
+    '''For coverage'''
+    exit(0)
+
+def defaultHandler(err):
+    response = err.get_response()
+    print('response', err, err.get_response())
+    response.data = dumps({
+        "code": err.code,
+        "name": "System Error",
+        "message": err.get_description(),
+    })
+    response.content_type = 'application/json'
+    return response
+
+APP = Flask(__name__)
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.register_error_handler(Exception, defaultHandler)
+
+#### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
 @APP.route('/auth/register/v2', methods = ['POST'])
 
@@ -53,6 +74,12 @@ def user_logout():
     token_decoded = token_decode(token)
     resp = auth_logout_v1(token_decoded)
     return dumps(resp)
-    
+
+#### NO NEED TO MODIFY BELOW THIS POINT
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, quit_gracefully) # For coverage
+    APP.run(port=config.port) # Do not edit this port
+
 
     
