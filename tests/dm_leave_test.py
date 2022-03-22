3992223ef3b3362c825145d8fb59dd3b3fac6620
Richard Xue <vinegar@Richards-MBP.hub>
2022-03-23 02:47:50 +1100

    V1 of dm leave tests

---
+++ tests/dm_leave_test.py
@@ -0,0 +1,166 @@
+import pytest
+import requests
+from src import config
+
+
+'''
+dm_data_structure = {
+    # Name of dm automatically generated
+    'name' : 'string',
+
+
+    # List of normal member u_ids
+    # Normal members == NOT owners
+    'normal_members' : [],
+
+
+    # List of owner u_ids
+    # Original creator is first
+    'owner' : [],
+
+    # List of message dictionaries
+    'messages' : [
+        {
+
+            # Message id is now the global message id
+            'message_id' : int,
+
+            # U_id of the sender
+            'sender_id' : int(u_id),
+
+            # Actual string of message
+            'message' : 'string',
+
+            # Import time function
+            'time_sent' : float(???)
+        }
+    ]
+}
+'''
+
+
+@pytest.fixture
+def post_test_user():
+    requests.delete(f"{config.url}/clear/v1")
+    '''
+    Creates a test user and posts for use in http testing.
+    '''
+    post_test_user = requests.post(f"{config.url}/auth/register/v2", json={
+        'email': 'user@gmail.com',
+        'password': 'password',
+        'name_first': 'FirstName',
+        'name_last': 'LastName',
+    })
+    user_data = post_test_user.json()
+    return user_data
+
+
+@pytest.fixture
+def post_dm_create():
+    post_info = post_test_user()
+    george_info = post_george()
+    bob_info = post_bob()
+    requests.post(f'{config.url}dm/create/v1', json={
+        'token': post_test_user['token'],
+        'u_ids': [bob_info['auth_user_id'], george_info['auth_user_id']]
+    })
+
+    return post_info.json()
+
+
+@pytest.fixture
+def fixture_george():
+    return post_george()
+
+
+@pytest.fixture
+def fixture_bob():
+    return post_bob()
+
+
+def post_george():
+    '''
+    Creates test user named George Monkey, email george@gmail.com.
+    '''
+    post_george = requests.post(f"{config.url}/auth/register/v2", json={
+        'email': 'george@gmail.com',
+        'password': 'monkey',
+        'name_first': 'George',
+        'name_last': 'Monkey',
+    })
+    george_data = post_george.json()
+    return george_data
+
+
+def post_bob():
+    '''
+    Creates test user named Bob Builder, email canwefixit@gmail.com .
+    '''
+    post_bob = requests.post(f"{config.url}/auth/register/v2", json={
+        'email': 'canwefixit@gmail.com',
+        'password': 'yeswecan',
+        'name_first': 'Bob',
+        'name_last': 'Builder',
+    })
+    bob_data = post_bob.json()
+    return bob_data
+
+
+def test_dm_leave_success(post_test_user, fixture_bob, fixture_george):
+
+    dm_id = requests.post(f'{config.url}dm/create/v1', json={
+        'token': post_test_user['token'],
+        'u_ids': [fixture_bob['auth_user_id'], fixture_george['auth_user_id']]
+    })
+
+    dm_details = requests.post(f'{config.url}dm/leave/v1', json={
+        'token': post_test_user['token'],
+        'dm_id': dm_id
+    })
+
+    assert dm_details.status_code == 200
+
+
+def test_dm_leave_invalid_token(post_test_user, fixture_bob, fixture_george):
+
+    dm_id = requests.post(f'{config.url}dm/create/v1', json={
+        'token': post_test_user['token'],
+        'u_ids': [fixture_bob['auth_user_id'], fixture_george['auth_user_id']]
+    })
+
+    dm_details = requests.post(f'{config.url}dm/leave/v1', json={
+        'token': 'inogneoieohgoiheohgohoehogheo',
+        'dm_id': dm_id
+    })
+
+    assert dm_details.status_code == 403
+
+
+def test_dm_leave_invalid_dm_id(post_test_user, fixture_bob, fixture_george):
+
+    requests.post(f'{config.url}dm/create/v1', json={
+        'token': post_test_user['token'],
+        'u_ids': [fixture_bob['auth_user_id'], fixture_george['auth_user_id']]
+    })
+
+    dm_details = requests.post(f'{config.url}dm/leave/v1', json={
+        'token': post_test_user['token'],
+        'dm_id': 999999999
+    })
+
+    assert dm_details.status_code == 400
+
+
+def test_dm_leave_non_member(post_test_user, fixture_bob, fixture_george):
+
+    dm_id = requests.post(f'{config.url}dm/create/v1', json={
+        'token': post_test_user['token'],
+        'u_ids': [fixture_bob['auth_user_id']]
+    })
+
+    dm_details = requests.post(f'{config.url}dm/leave/v1', json={
+        'token': fixture_george['token'],
+        'dm_id': dm_id
+    })
+
+    assert dm_details.status_code == 403
