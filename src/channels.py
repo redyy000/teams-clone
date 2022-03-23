023ea000fd13e20a355558be1814a370d1923979
Daniel Lin <daniel.lin111@hotmail.com>
2022-03-23 16:43:02 +1100

    removed dup

--- src/channels.py
+++ src/channels.py
@@ -1,27 +1,32 @@
 from src.data_store import data_store
 from src.error import InputError
 from src.error import AccessError
-
-'''
-Provide a list of all channels (and their associated details) that the authorised user is part of.
-
-Arguments:
-    auth_user_id (integer)  - an authorisation hash of the user
-
-Exceptions:
-    AccessError     - Occurs when auth_user_id is invalid.
-
-Return Value:
-    'channels'      - A list of dictionaries containing channel ID's and names for each channel they are a part of 
-
-'''
+from src.other import load_data, store_data, is_valid_token
 
 
-def channels_list_v1(auth_user_id):
-    store = data_store.get()
-    if isinstance(auth_user_id, int) != True or auth_user_id <= 0 or auth_user_id > len(store['users']):
+def channels_list_v2(auth_user_id):
+    '''
+    Provide a list of all channels (and their associated details) that the authorised user is part of.
+
+    Arguments:
+        auth_user_id (integer)  - an authorisation hash of the user
+
+    Exceptions:
+        AccessError     - Occurs when auth_user_id is invalid.
+
+    Return Value:
+        'channels'      - A list of dictionaries containing channel ID's and names for each channel they are a part of 
+
+    '''
+    store = load_data()
+
+    token_decoded = is_valid_token(token)
+    if token_decoded == False:
         raise AccessError(
-            f"User ID {auth_user_id} is invalid. Unable to access any details with this ID.")
+            description=f"User ID {auth_user_id} is invalid. Unable to access any details with this ID.")
+
+    auth_user_id = token_decoded["u_id"]
+
     channel_details = []
     # For each channel in the list of channels
     for channel in store['channels']:
@@ -33,32 +38,30 @@ def channels_list_v1(auth_user_id):
                     'name': channel['name']
                 }
                 channel_details.append(channel_info)
-        data_store.set(store)
+        store_data(store)
     return {
         'channels': channel_details
     }
 
 
-'''
-Provide a list of all channels, including private channels, (and their associated details)
+def channels_listall_v2(token):
+    '''
+    Provide a list of all channels, including private channels, (and their associated details)
 
-Arguments:
-    auth_user_id (integer)  - an authorisation hash of the user
+    Arguments:
+        token           - A JWT.
 
-Exceptions:
-    AccessError     - Occurs when auth_user_id is invalid.
+    Exceptions:
+        AccessError     - Occurs when auth_user_id is invalid.
 
-Return Value:
-    'channels'      - A list of dictionaries containing channel ID's and names for each channel that exists 
+    Return Value:
+        'channels'      - A list of dictionaries containing channel ID's and names for each channel that exists 
 
-'''
-
-
-def channels_listall_v1(auth_user_id):
-    store = data_store.get()
-    if isinstance(auth_user_id, int) != True or auth_user_id <= 0 or auth_user_id > len(store['users']):
-        raise AccessError(
-            f"User ID {auth_user_id} is invalid. Unable to access any details with this ID.")
+    '''
+    token_decoded = is_valid_token(token)
+    if token_decoded == False:
+        raise AccessError(description='False Token!')
+    store = load_data()
     channel_details = []
     # For each channel in the list of channels
     for channel in store['channels']:
@@ -67,13 +70,12 @@ def channels_listall_v1(auth_user_id):
             'name': channel['name']
         }
         channel_details.append(channel_info)
-        data_store.set(store)
     return {
         'channels': channel_details
     }
 
 
-def channels_create_v1(auth_user_id, name, is_public):
+def channels_create_v2(token, name, is_public):
     '''
     Function to create a named channel either private or public
     Arguments:
@@ -87,10 +89,19 @@ def channels_create_v1(auth_user_id, name, is_public):
         InputError   -  Occurs when public status return is invalid. 
     Return Value:
         a dictionary {channel_id}
-    '''
-    store = data_store.get()
 
-    channel_id = len(store['channels']) + 1
+    '''
+    # Check token is valid
+    data = load_data()
+
+    #  Check for valid user ID i.e. token
+    token_decoded = is_valid_token(token)
+    if token_decoded == False:
+        raise AccessError(description="Invalid Token")
+
+    auth_user_id = token_decoded["u_id"]
+
+    channel_id = len(data['channels']) + 1
 
     new_channel = {'channel_id': channel_id,
                    'name': name,
@@ -103,30 +114,25 @@ def channels_create_v1(auth_user_id, name, is_public):
                    'messages': []
                    }
 
-    #  Check for valid user ID
-    if isinstance(auth_user_id, int) != True or auth_user_id < 1 or auth_user_id > len(store['users']):
-        raise AccessError(
-            f"User ID {auth_user_id} is invalid. Unable to create channel with this ID.")
-
     # Check for valid channel name, between 1 and 20 characters
     if len(name) == 0 or len(name) > 20:
         raise InputError(
-            "No channel name is entered or channel name is longer than 20 characters.")
+            description="No channel name is entered or channel name is longer than 20 characters.")
 
     # Check for duplicate channel names
-    for channel in store['channels']:
+    for channel in data['channels']:
         if channel['name'] == new_channel['name']:
             raise InputError(
-                "This channel name already exists, please create a new channel name or request to join this channel.")
+                description="This channel name already exists, please create a new channel name or request to join this channel.")
 
     # Check is_public returns boolean
     if type(is_public) != bool:
         raise InputError(
-            "Something went wrong, unable to determine if channel is public or private.")
+            description="Something went wrong, unable to determine if channel is public or private.")
 
     # Append the new channel to the list of channels
-    store['channels'].append(new_channel)
-    data_store.set(store)
+    data['channels'].append(new_channel)
+    store_data(data)
 
     return {
         'channel_id': channel_id,
--- src/server.py
+++ src/server.py
@@ -1,15 +1,44 @@
 from flask import Flask, request, abort
-from auth import auth_register_v2, auth_login_v2, auth_logout_v1
+from src.auth import auth_register_v2, auth_login_v2, auth_logout_v1
 from src.error import AccessError
 from src.other import is_valid_token
-from other import token_create, token_decode, is_valid_token
 from json import dumps
-import jwt
 from src.other import is_valid_token
 from src.channel import channel_invite_v1, channel_join_v1, channel_messages_v1
+from flask_cors import CORS
+from src.user import user_profile_v1, user_profile_setemail_v1, user_profile_sethandle_v1, user_profile_setname_v1
+from src.channels import channels_create_v2, channels_list_v2, channels_listall_v2
+from src.other import clear_v1
+from src.config import port
+import signal
+
+
+def quit_gracefully(*args):
+    '''For coverage'''
+    exit(0)
+
+
+def defaultHandler(err):
+    response = err.get_response()
+    print('response', err, err.get_response())
+    response.data = dumps({
+        "code": err.code,
+        "name": "System Error",
+        "message": err.get_description(),
+    })
+    response.content_type = 'application/json'
+    return response
+
 
 APP = Flask(__name__)
-SECRET = 'PLACEHOLDER'
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.register_error_handler(Exception, defaultHandler)
+
+# NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
+
+SECRET = 'RICHARDRYANDANIELMAXTAYLA'
 
 
 @APP.route("/channel/join/v2", methods=['POST'])
@@ -55,3 +84,104 @@ def channel_messages():
     returnvalue = channel_messages_v1(user_id, channel_id, start)
 
     return dumps(returnvalue)
+
+
+@APP.route("/auth/register/v2", methods=['POST'])
+def auth_register():
+    arguments = request.get_json()
+    # user_data is of form:
+    '''
+    user = {
+        'u_id'  : auth_user_id,
+        'email' : email,
+        'password'   : hash(password),
+        'name_first' : name_first,
+        'name_last'  : name_last,
+        'handle_str' : create_handle_str(store, name_first, name_last),
+        'session_id_list' : [1]
+    }
+    '''
+    # What happens when auth_register_v2 throws an exception?
+    resp = auth_register_v2(arguments['email'],
+                            arguments['password'],
+                            arguments['name_first'],
+                            arguments['name_last'])
+    return dumps(resp)
+
+
+@APP.route("/auth/login/v2", methods=['POST'])
+def auth_login():
+    arguments = request.get_json()
+    resp = auth_login_v2(arguments['email'], arguments['password'])
+    return dumps(resp)
+
+
+@APP.route("/auth/logout/v1", methods=['POST'])
+def auth_logout():
+    arguments = request.get_json()
+    resp = auth_logout_v1(arguments['token'])
+    return dumps(resp)
+
+
+@APP.route("/channels/create/v2", methods=["POST"])
+def channels_create():
+    arguments = request.get_json()
+    resp = channels_create_v2(
+        arguments["token"], arguments["name"], arguments["is_public"])
+    return dumps(resp)
+
+
+@APP.route("/channels/list/v2", methods=["GET"])
+def channels_list():
+    token = request.args.get('token')
+    resp = channels_list_v2(token)
+    return dumps(resp)
+
+
+@APP.route("/clear/v1", methods=["DELETE"])
+def clear():
+    clear_v1()
+    return dumps({})
+
+
+@APP.route("/user/profile/v1", methods=['GET'])
+def user_profile_get():
+    arguments = request.get_json()
+    resp = user_profile_v1(arguments['token'], arguments['u_id'])
+    return dumps(resp)
+
+
+@APP.route("/user/profile/setemail/v1", methods=['PUT'])
+def user_profile_setemail():
+    arguments = request.get_json()
+    resp = user_profile_setemail_v1(arguments['token'], arguments['email'])
+    return dumps(resp)
+
+
+@APP.route("/user/profile/sethandle/v1", methods=['PUT'])
+def user_profile_sethandle():
+    arguments = request.get_json()
+    resp = user_profile_sethandle_v1(
+        arguments['token'], arguments['handle_str'])
+    return dumps(resp)
+
+
+@APP.route("/user/profile/setname/v1", methods=['PUT'])
+def user_profile_setname():
+    arguments = request.get_json()
+    resp = user_profile_setname_v1(
+        arguments['token'], arguments['name_first'], arguments['name_last'])
+    return dumps(resp)
+
+
+@APP.route("/channels/listall/v2", methods=['GET'])
+def channels_listall():
+    token = request.args.get('token')
+    returnvalue = channels_listall_v2(token)
+    return dumps(returnvalue)
+
+
+# NO NEED TO MODIFY BELOW THIS POINT
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
+    APP.run(port=port)  # Do not edit this port
