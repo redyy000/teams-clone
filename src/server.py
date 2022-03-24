698c72c7508aed5436b08105d5c77e53a8c77120
Richard Xue <richard.xue1@student.unsw.edu.au>
2022-03-24 23:39:09 +1100

    Update server.py to include admin stuff

--- src/server.py
+++ src/server.py
@@ -16,6 +16,7 @@ from src.channels import channels_create_v2, channels_list_v2, channels_listall_
 from src.channel import channel_details_v2, channel_invite_v2, channel_join_v2, channel_messages_v2, channel_leave_v1, channel_addowner_v1
 from src.dm import dm_create_v1, dm_details_v1, dm_list_v1, dm_remove_v1, dm_leave_v1, dm_messages_v1
 from src.message import message_send_v1, message_senddm_v1, message_remove_v1, message_edit_v1
+from src.admin import admin_userpermission_change_v1
 
 
 # dm_messages_v1
@@ -289,6 +290,15 @@ def message_remove():
     arguments = request.get_json()
     resp = message_remove_v1(
         arguments['token'], arguments['message_id'])
+
+    return dumps(resp)
+
+
+@APP.route("/admin/userpermission/change/v1", methods=['POST'])
+def admin_permission_change():
+    arguments = request.get_json()
+    resp = admin_userpermission_change_v1(
+        arguments['token'], arguments['u_id'], arguments['permission_id'])
 
     return dumps(resp)
 
