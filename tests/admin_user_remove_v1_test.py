7987e58f31bdee9605b20073c1771126c13fad4f
Richard Xue <vinegar@Richards-MBP.hub>
2022-03-26 00:20:36 +1100

    Added more remove tests to reach 100% coverage

--- tests/admin_user_remove_v1_test.py
+++ tests/admin_user_remove_v1_test.py
@@ -1,3 +1,4 @@
+from math import remainder
 import pytest
 import requests
 from src import config
@@ -104,6 +105,7 @@ def test_admin_user_remove_token_non_authorised(setup_users):
 def test_admin_user_remove_from_channel(setup_users):
     owner = setup_users[0]
     member1 = setup_users[1]
+    member2 = setup_users[2]
 
     # Create a channel for owner
     channel_response = requests.post(f"{config.url}channels/create/v2", json={
@@ -119,6 +121,21 @@ def test_admin_user_remove_from_channel(setup_users):
 
     assert join_response.status_code == 200
 
+    # Member 2 Joins
+    join2_response = requests.post(f'{config.url}channel/join/v2', json={
+        'token': member2['token'],
+        'channel_id': channel_response.json()['channel_id']
+    })
+
+    assert join2_response.status_code == 200
+
+    # Member 2 posts messages
+    requests.post(f"{config.url}message/send/v1", json={
+        "token": member2['token'],
+        "channel_id": channel_response.json()['channel_id'],
+        "message": 'Sekiro is better than elden ring'
+    })
+
     # Member 1 posts messages
     requests.post(f"{config.url}message/send/v1", json={
         "token": member1['token'],
@@ -131,14 +148,23 @@ def test_admin_user_remove_from_channel(setup_users):
         "channel_id": channel_response.json()['channel_id'],
         "message": 'PUT YOUR AMBITIONS TO REST'
     })
-
-    remove_response = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+    # Ban member 1
+    remove_response1 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
         'token': owner['token'],
         'u_id': member1['auth_user_id'],
     })
 
-    assert remove_response.status_code == 200
-    assert remove_response.json() == {}
+    # Ban member 2
+    remove_response2 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+        'token': owner['token'],
+        'u_id': member2['auth_user_id'],
+    })
+
+    assert remove_response1.status_code == 200
+    assert remove_response1.json() == {}
+
+    assert remove_response2.status_code == 200
+    assert remove_response2.json() == {}
 
     # Assert messages are all turned into 'Removed user'
 
@@ -151,6 +177,63 @@ def test_admin_user_remove_from_channel(setup_users):
     message_list = messages_response.json()['messages']
     for message_dict in message_list:
         assert message_dict['message'] == 'Removed user'
+
+
+def test_admin_user_remove_from_channel_owner(setup_users):
+    owner = setup_users[0]
+    member1 = setup_users[1]
+    member2 = setup_users[2]
+
+    # Create a channel for owner
+    channel_response = requests.post(f"{config.url}channels/create/v2", json={
+        "token": owner['token'],
+        "name": "general",
+        "is_public": True
+    })
+
+    # Member 1 Joins
+    join1_response = requests.post(f'{config.url}channel/join/v2', json={
+        'token': member1['token'],
+        'channel_id': channel_response.json()['channel_id']
+    })
+
+    assert join1_response.status_code == 200
+
+    # Member 2 Joins
+    join2_response = requests.post(f'{config.url}channel/join/v2', json={
+        'token': member2['token'],
+        'channel_id': channel_response.json()['channel_id']
+    })
+
+    assert join2_response.status_code == 200
+
+    # Add member 1 and 2 as owners
+    add_owner_response1 = requests.post(f"{config.url}channel/addowner/v1", json={
+        'token': owner['token'],
+        'channel_id': channel_response.json()['channel_id'],
+        'user_id': member1['auth_user_id']})
+    assert add_owner_response1.status_code == 200
+
+    add_owner_response2 = requests.post(f"{config.url}channel/addowner/v1", json={
+        'token': owner['token'],
+        'channel_id': channel_response.json()['channel_id'],
+        'user_id': member2['auth_user_id']})
+    assert add_owner_response2.status_code == 200
+
+    # Ban member 2 :(
+
+    remove_response2 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+        'token': owner['token'],
+        'u_id': member2['auth_user_id'],
+    })
+    # Ban member 1 :(
+    remove_response1 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+        'token': owner['token'],
+        'u_id': member2['auth_user_id'],
+    })
+
+    assert remove_response1.status_code == 200
+    assert remove_response2.status_code == 200
 
 
 def test_admin_user_remove_from_dm(setup_users):
@@ -196,3 +279,48 @@ def test_admin_user_remove_from_dm(setup_users):
     message_list = messages_response.json()['messages']
     for message_dict in message_list:
         assert message_dict['message'] == 'Removed user'
+
+
+def test_admin_user_remove_from_dm_owner(setup_users):
+    owner = setup_users[0]
+    member1 = setup_users[1]
+    member2 = setup_users[2]
+
+    # Create DM, where member2 is the dm owner
+    dm_response = requests.post(f"{config.url}dm/create/v1", json={
+        "token": member2['token'],
+        'u_ids': [owner['auth_user_id'], member1['auth_user_id']]
+    })
+
+    assert dm_response.status_code == 200
+    # Member 1 posts messages
+    requests.post(f"{config.url}message/senddm/v1", json={
+        "token": member1['token'],
+        "dm_id": dm_response.json()['dm_id'],
+        "message": 'Every soul has its dark'
+    })
+
+    requests.post(f"{config.url}message/senddm/v1", json={
+        "token": member1['token'],
+        "dm_id": dm_response.json()['dm_id'],
+        "message": 'PUT YOUR AMBITIONS TO REST'
+    })
+    # Member 2 sends messages
+    requests.post(f"{config.url}message/senddm/v1", json={
+        "token": member2['token'],
+        "dm_id": dm_response.json()['dm_id'],
+        "message": 'gjeohgeoijhgoiehoihgoihoiehg wow'
+    })
+    # Ban member 2 :(
+    remove_response2 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+        'token': owner['token'],
+        'u_id': member2['auth_user_id'],
+    })
+    # Ban member 1 :(
+    remove_response1 = requests.delete(f"{config.url}/admin/user/remove/v1", json={
+        'token': owner['token'],
+        'u_id': member2['auth_user_id'],
+    })
+
+    assert remove_response1.status_code == 200
+    assert remove_response2.status_code == 200
