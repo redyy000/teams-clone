c82ba690ab989befd9327d7e51205eb8f4ba01ab
Tayla Slater <t.slater@student.unsw.edu.au>
2022-04-06 14:36:16 +1000

    updated server.py with search_v1 route

--- src/server.py
+++ src/server.py
@@ -11,7 +11,7 @@ from urllib import response
 from flask import Flask, request, abort
 from flask_cors import CORS
 from src.auth import auth_register_v2, auth_login_v2, auth_logout_v1
-from src.other import clear_v1
+from src.other import clear_v1, search_v1
 from src.channels import channels_create_v2, channels_list_v2, channels_listall_v2
 from src.channel import channel_details_v2, channel_invite_v2, channel_join_v2, channel_messages_v2, channel_leave_v1, channel_addowner_v1, channel_removeowner_v1
 from src.dm import dm_create_v1, dm_details_v1, dm_list_v1, dm_remove_v1, dm_leave_v1, dm_messages_v1
@@ -340,6 +340,16 @@ def admin_user_remove():
     arguments = request.get_json()
     resp = admin_user_remove_v1(
         arguments['token'], arguments['u_id'])
+    data_store.save()
+    return dumps(resp)
+
+
+@APP.route("/search/v1", methods=['GET'])
+def search():
+    token = request.args.get('token', type=str)
+    query_str = request.args.get('query_str', type=str)
+    resp = search_v1(
+        token, query_str)
     data_store.save()
     return dumps(resp)
 
