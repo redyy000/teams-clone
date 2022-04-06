c36e37eb16d02aedaec57c61a74df11590ead8fd
Tayla Slater <t.slater@student.unsw.edu.au>
2022-04-06 14:36:40 +1000

    Updated other.py with search_v1 code

--- src/other.py
+++ src/other.py
@@ -1,6 +1,7 @@
 import json
 import jwt
 from src.data_store import data_store, initial_object
+from src.error import InputError, AccessError
 
 SECRET = "RICHARDRYANDANIELMAXTAYLA"
 
@@ -74,3 +75,55 @@ def is_valid_token(token):
             if user['session_id_list'].count(payload['session_id']) != 0:
                 return payload
         return False
+
+
+def search_v1(token, query_str):
+    '''
+    Given a query string, return a collection of messages in all of the channels/DMs that the user has joined that match the query
+    Arguments:
+        token (string)      - an authorisation hash of the user conducting the search
+        query_str (string)  - case insensitive search query
+    Exceptions:
+        InputError  - query string is more than 1000 characters
+        AccessError - token is invalid
+    Return Value:
+        Returns {messages}
+    '''
+    decoded_token = is_valid_token(token)
+    if decoded_token is False:
+        raise AccessError(description='Token not authorised to search.')
+
+    if len(query_str) > 1000:
+        raise InputError(
+            description='Query string too long, 1000 characters or under only.')
+
+    if len(query_str) < 1:
+        raise InputError(description='Query string empty.')
+
+    message_list = []
+
+    store = data_store.get()  # check this is how we are still retrieving data
+
+    for channel in store['channels']:
+        is_in_channel = False
+        for member in channel['all_members']:
+            if member['u_id'] == decoded_token['u_id']:
+                is_in_channel = True
+                break
+        if is_in_channel:
+            for channel_message in channel['messages']:
+                if query_str in channel_message['message']:
+                    message_list.append(channel_message)
+
+    for dm in store['dms']:
+        is_in_dm = False
+        if decoded_token['u_id'] in dm['members']:
+            is_in_dm = True
+        if is_in_dm:
+            for dm_message in dm['messages']:
+                if query_str in dm_message['message']:
+                    message_list.append(dm_message)
+
+    return {
+        'messages': message_list
+    }
