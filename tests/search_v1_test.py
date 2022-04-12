d27761a839226cfa1d241a2b3aca902fe9f05b13
Tayla Slater <t.slater@student.unsw.edu.au>
2022-04-12 15:00:16 +1000

    removed code we don't need, passes tests

--- tests/search_v1_test.py
+++ tests/search_v1_test.py
@@ -15,6 +15,7 @@ from src import config
 
 @pytest.fixture
 def setup_users():
+    requests.delete(f'{config.url}clear/v1')
     userlist = []
     response1 = requests.post(f'{config.url}auth/register/v2', json={'email': "dlin@gmail.com",
                                                                      'password': "password",
@@ -40,57 +41,7 @@ def setup_users():
     return userlist
 
 
-# Admin creates a public channel, invites user 1, messages are sent:
-# @pytest.fixture
-# def setup_channel(setup_users):
-#     owner = setup_users[0]
-#     user1 = setup_users[1]
-#     channel = requests.post(f'{config.url}channels/create/v2', json={
-#         'token': owner['token'],
-#         'name': "Public",
-#         'is_public': True
-#     }).json()
-#     requests.post(f'{config.url}channel/invite/v2', json={
-#         'token': owner['token'],
-#         'channel_id': channel['channel_id'],
-#         'u_id': user1['auth_user_id']
-#     })
-#     requests.post(f"{config.url}message/send/v1", json={
-#         "token": owner['token'],
-#         "channel_id": channel['channel_id'],
-#         "message": "Test channel message first"
-#     })
-#     requests.post(f"{config.url}message/send/v1", json={
-#         "token": user1['token'],
-#         "channel_id": channel['channel_id'],
-#         "message": "Test channel message second"
-#     })
-
-
-# Admin creates a dm with users 1 and 2, sends some messages
-# @pytest.fixture
-# def setup_dm(setup_users):
-#     owner = setup_users[0]
-#     user1 = setup_users[1]
-#     user2 = setup_users[2]
-#     new_dm = requests.post(f'{config.url}dm/create/v1', json={
-#         'token': owner['token'],
-#         'u_ids': [user1['auth_user_id'], user2['auth_user_id']]
-#     }).json()
-#     requests.post(f'{config.url}message/senddm/v1', json={
-#         'token': owner['token'],
-#         'dm_id': new_dm['dm_id'],
-#         'message': "Test dm message first"
-#     })
-#     requests.post(f'{config.url}message/senddm/v1', json={
-#         'token': user2['token'],
-#         'dm_id': new_dm['dm_id'],
-#         'message': "Test dm message second"
-#     })
-
-
 def test_search_success(setup_users):
-    requests.delete(f"{config.url}/clear/v1")
     owner = setup_users[0]
     user1 = setup_users[1]
     user2 = setup_users[2]
@@ -100,21 +51,22 @@ def test_search_success(setup_users):
         'token': owner['token'],
         'name': "Public Channel",
         'is_public': True
-    }).json()
+    })
+    assert channel1.status_code == 200
 
     requests.post(f'{config.url}channel/invite/v2', json={
         'token': owner['token'],
-        'channel_id': channel1['channel_id'],
+        'channel_id': channel1.json()['channel_id'],
         'u_id': user1['auth_user_id']
     })
     requests.post(f"{config.url}message/send/v1", json={
         "token": owner['token'],
-        "channel_id": channel1['channel_id'],
+        "channel_id": channel1.json()['channel_id'],
         "message": "Test channel message first"
     })
     requests.post(f"{config.url}message/send/v1", json={
         "token": user1['token'],
-        "channel_id": channel1['channel_id'],
+        "channel_id": channel1.json()['channel_id'],
         "message": "Test channel message second"
     })
 
@@ -135,10 +87,12 @@ def test_search_success(setup_users):
     })
 
     search = requests.get(f'{config.url}/search/v1', params={
-        'token': owner['token'], 'query_str': 'first'}).json()
+        'token': owner['token'], 'query_str': 'first'})
+
+    assert search.status_code == 200
 
     messages_list = []
-    for message in search['messages']:
+    for message in search.json()['messages']:
         messages_list.append(message['message'])
 
     assert 'Test channel message first' in messages_list
@@ -146,24 +100,21 @@ def test_search_success(setup_users):
     assert len(messages_list) == 2
 
 
-# def test_invalid_token():
-#     requests.delete(f"{config.url}/clear/v1")
-#     response_invalid = requests.get(f'{config.url}/search/v1', params={
-#         'token': 'invalid_token', 'query_str': 'searching messages...'})
-#     assert response_invalid.status_code == 403
+def test_invalid_token():
+    response_invalid = requests.get(f'{config.url}/search/v1', params={
+        'token': 'invalid_token', 'query_str': 'searching messages...'})
+    assert response_invalid.status_code == 403
 
 
-# def test_query_string_short(setup_users):
-#     requests.delete(f"{config.url}/clear/v1")
-#     owner = setup_users[0]
-#     response = requests.get(f'{config.url}/search/v1', params={
-#         'token': owner['token'], 'query_str': ''})
-#     assert response.status_code == 400
+def test_query_string_short(setup_users):
+    owner = setup_users[0]
+    response = requests.get(f'{config.url}/search/v1', params={
+        'token': owner['token'], 'query_str': ''})
+    assert response.status_code == 400
 
 
-# def test_query_string_long(setup_users, setup_channel):
-#     requests.delete(f"{config.url}/clear/v1")
-#     owner = setup_users[0]
-#     response = requests.get(f'{config.url}/search/v1', params={
-#         'token': owner['token'], 'query_str': 200 * 'This is too long'})
-#     assert response.status_code == 400
+def test_query_string_long(setup_users):
+    owner = setup_users[0]
+    response = requests.get(f'{config.url}/search/v1', params={
+        'token': owner['token'], 'query_str': 200 * 'This is too long'})
+    assert response.status_code == 400
