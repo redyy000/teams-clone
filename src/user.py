09f2b4e2164d7a4f3e23eaf1f567b2a9b0f1acd5
Richard Xue <vinegar@Richards-MBP.hub>
2022-04-16 02:23:49 +1000

    fixed false url bug

--- src/user.py
+++ src/user.py
@@ -345,11 +345,11 @@ def user_profile_uploadphoto_v1(token, img_url, x_start, y_start, x_end, y_end):
     img_file = 'src/' + 'static/' + random_string + '.jpg'
 
     # Download image
-    try:
-        urllib.request.urlretrieve(img_url, img_file)
-    except InputError as failure:
+    urllib.request.urlretrieve(img_url, img_file)
+    web_url = urllib.request.urlopen(img_url)
+    if web_url.getcode() != 200:
         raise InputError(
-            description='Failed while retrieving image from URL!') from failure
+            description='Failed while retrieving image from URL!')
 
     # Cropping part
     imageObject = Image.open(img_file)
